f6f3b1ec539d4ac4d362ac5ee1175cb8c2884f67
Nicholas Patty <nicho@NPs-MacBook.local>
2025-09-25 10:49:33 -0600

    fix curl and ci

--- test/func/test_add.sh
+++ test/func/test_add.sh
@@ -1,4 +1,4 @@
-test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+test -e ssshtest || curl -s -O https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
 #$ python mymath.py add 1 1
@@ -11,3 +11,4 @@ assert_exit_code 0
 run test_add_ints_2 python src/mymath.py div 1 0
 assert_in_stdout "Error: Division by zero"
 assert_exit_code 1
+
